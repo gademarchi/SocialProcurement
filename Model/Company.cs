d0f4ad9774e3573a8e881f408bff65367e4fe7c0
gademarchi <gadmarchi@gmail.com>
2017-11-29 17:30:12 -0500

    Create the Solution

---
+++ Model/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common
+
+namespace Model{
+    public class Company
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        
+        public String Email { get; set; }
+    }
+}
---
+++ Model/SpContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Model
+{
+    public class SpContex : DbContext
+    {
+
+
+        public virtual DbSet<Company> Companies { get; set; }
+
+        public SpContex(): base("name=Model.SpContex")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
